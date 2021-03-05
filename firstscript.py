f71b1a0ca5e35e7380efbf59887f6d40a83b0def
cthacker-udel <cthacker@udel>
2021-03-05 06:57:21 -0500

    fifth commit

--- firstscript.py
+++ firstscript.py
@@ -39,11 +39,45 @@ def random_coord():
 def get_the_pixel(x,y):
     return sense.get_pixel(x,y)
 
+def clear_hat():
+    sense.clear()
+
+def clear_hat_color(r,g,b):
+    sense.clear(r,g,b)
+
+def scroll_message_default(amessage,afloat=None,text_color=None,back_color=None):
+    if afloat != None and text_color != None and back_color != None:
+        sense.show_message("first if")
+        sense.show_message(amessage,afloat,text_color,back_color)
+    elif afloat != None and text_color != None:
+        sense.show_message("second_if")
+        sense.show_message(amessage,afloat,text_color)
+    elif afloat != None and text_color == None and back_color != None:
+        sense.show_message("third if")
+        sense.show_message(amessage,afloat,text_color,back_color)
+    elif afloat == None and text_color != None and back_color != None:
+        sense.show_message("fourth if")
+        sense.show_message(amessage,afloat,text_color,back_color)
+    else:
+        sense.show_message(amessage)
+
+def display_letter(aletter,textcolor=None,backcolor=None):
+    if textcolor != None and backcolor != None:
+        sense.show_letter(aletter,textcolor,backcolor)
+    elif textcolor != None:
+        sense.show_letter(aletter,textcolor)
+    elif backcolor != None:
+        sense.show_letter(aletter,backcolor)
+    else:
+        sense.show_letter(aletter)
 
 incrementer = 0
 while incrementer <= 270:
+    display_letter('a')
+    display_letter('w',[255,0,0])
    # flip_vertical()
-    sense.set_pixel(random_coord(),random_coord(),random_pixel_number(),random_pixel_number(),random_pixel_number())
+   # scroll_message_default("Message",2.5,[255,0,0])
+   # sense.set_pixel(random_coord(),random_coord(),random_pixel_number(),random_pixel_number(),random_pixel_number())
    # sense.set_pixels(generate_pixel_list())
    # sense.set_rotation(incrementer)
    # sense.show_message("Hello world!")
