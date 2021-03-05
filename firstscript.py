5114013b4f7c8830627b9a6d5230ea1e6287c940
cthacker-udel <cthacker@udel>
2021-03-05 06:31:47 -0500

    fourth commit

--- firstscript.py
+++ firstscript.py
@@ -16,9 +16,23 @@ def flip_horizontal():
 def undo_horizontal():
     sense.flip_h(False)
 
+def random_pixel_number():
+    return random.randint(0,255)
+
+def random_pixel_number_list():
+    return [x for x in range(0,256)]
+
+def generate_pixel_list():
+    x = random_pixel_number_list()
+    return [[random.choice(x),random.choice(x),random.choice(x)] for y in range(64)]
+
+def get_the_pixels():
+    return sense.get_pixels()
+
 incrementer = 0
 while incrementer <= 270:
-    flip_vertical()
-    sense.set_rotation(incrementer)
-    sense.show_message("Hello world!")
+   # flip_vertical()
+    sense.set_pixels(generate_pixel_list())
+   # sense.set_rotation(incrementer)
+   # sense.show_message("Hello world!")
     incrementer += 90
