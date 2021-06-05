f6f4a8c8bbaf38688ee596598e640e6e47bb0ef4
cthacker-udel <cthacker@udel.edu>
2021-06-05 06:57:51 -0400

    Added folder for c++ practice

---
+++ cpp-practice/Project-Student/Student.hpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <ctype.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Student{
+
+    string name;
+    int age;
+    double height;
+
+
+    public:
+        
+        Student();
+
+        Student(string,int,double);
+
+        Student(string,int);
+
+        int getAge();
+
+        string getName();
+
+        double getHeight();
+
+};
+
+
