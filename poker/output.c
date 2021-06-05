780cccaeb45de92aaeff0b2d6d45f6b32d06398b
cthacker-udel <cthacker@udel.edu>
2021-06-05 00:57:29 -0400

    Updated output.c with colors to clear framebuffer upon function call of closeDisplay and also modified the closeDisplay function, formatting, and added the clearFrameBuffer method

--- poker/output.c
+++ poker/output.c
@@ -1,5 +1,15 @@
 #include "project.h"
 
+#define BLACK 0x0000
+#define WHITE 0xFFFF
+#define BLUE 0x1F
+#define RED 0xF800
+#define YELLOW 0xFFE0
+#define ORANGE 0xFBE0
+#define BROWN 0x79E0
+#define GREEN 0x7E0
+#define CYAN 0x7FF
+
 
 pi_framebuffer_t *fb = NULL;
 
@@ -18,11 +28,12 @@ void openDisplay(void){
 void closeDisplay(void){
 
    if(fb){
-    fb = NULL;
-    freeFrameBuffer(fb);
+        clearFrameBuffer(fb,BLACK);
+        freeFrameBuffer(fb);
+        fb = NULL;
    }
    else{
-    fprintf(stderr,"Attempting to close a framebuffer that has not been initialized or otherwise already closed");
+        fprintf(stderr,"Attempting to close a framebuffer that has not been initialized or otherwise already closed");
    }
 
 }
