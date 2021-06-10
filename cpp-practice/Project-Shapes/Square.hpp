ed07df5465d21187ac9bf7e365de5cda97185e8a
cthacker-udel <cthacker@udel.edu>
2021-06-10 05:49:48 -0400

    Added square class

---
+++ cpp-practice/Project-Shapes/Square.hpp
@@ -0,0 +1,27 @@
+
+
+class Square{
+
+    int sideLength;
+    int area;
+    int diagonal;
+    int perimeter;
+
+    public:
+
+        Square();
+
+        Square(int);
+
+        int getSideLength();
+
+        void setSideLength(int);
+
+        int getArea();
+
+        int getDiagonal();
+
+        int getPerimeter();
+
+
+};
