44082a9c2f5302305d755e533d34489e593bb198
cthacker-udel <cthacker@udel.edu>
2021-06-10 06:20:58 -0400

    Added RightTriangle classs

---
+++ cpp-practice/Project-Shapes/RightTriangle.hpp
@@ -0,0 +1,20 @@
+
+class RightTriangle{
+
+    int legA;
+    int legB;
+    int hypotenuse;
+
+    public:
+
+        RightTriangle();
+
+        RightTriangle(int,int);
+
+        int getArea();
+
+        int getPerimeter();
+        
+        int getHypotenuse;
+
+};
