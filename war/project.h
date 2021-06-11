1b8c05119247610e2cf3b7004772c5a4ab315003
cthacker-udel <cthacker@udel.edu>
2021-06-11 03:47:31 -0400

    Added base typedef definition in project.h file, and also added base Makefile

---
+++ war/project.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <math.h>
+
+
+typedef struct card{
+
+    int rank;
+    char suit[10];
+    char rank[10];
+
+
+}warCard;
+
+
