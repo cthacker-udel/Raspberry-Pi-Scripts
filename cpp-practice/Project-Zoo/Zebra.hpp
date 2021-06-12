01ae7c80927fffbe64f5fac8f223e286d9524d0b
cthacker-udel <cthacker@udel.edu>
2021-06-11 21:36:45 -0400

    Added functionality to Zebra.hpp file

---
+++ cpp-practice/Project-Zoo/Zebra.hpp
@@ -0,0 +1,22 @@
+
+class Zebra{
+
+	int legs;
+	int age;
+	string roar;
+
+
+	public:
+
+		int getLegNumber();
+
+		int getAge();
+
+		string getRoar();
+
+		void setAge(int);
+
+		void setRoar(string);
+
+
+};
