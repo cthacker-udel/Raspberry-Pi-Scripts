011d3928990b0c3cf3e224a7d4e7f437ff561b2c
cthacker-udel <cthacker@udel.edu>
2021-06-15 06:23:04 -0400

    Added implementation for odd number guessing game

---
+++ numberGuessingGames/OddNumber/number.c
@@ -0,0 +1,55 @@
+#include "project.h"
+
+int playerScore = 0;
+int computerScore = 0;
+
+int generateRandomNumber(){
+
+    long number = rand();
+    int twoNumber = rand() % number;
+    return twoNumber;
+
+}
+
+void displayScore(){
+
+    printf("\n!#!#!#!#SCORE DISPLAY!#!#!#!#\n\nPLAYER SCORE : %d\n\nCOMPUTER SCORE : %d\n\n!#!#!#!#!#!#!#!#\n",playerScore,computerScore);
+
+}
+
+void correctGuess(){
+
+    printf("\n\nTHE GUESS IS\n\n....");
+    printf("CORRECT!\n");
+    playerScore++;
+
+}
+
+void incorrectGuess(){
+
+    printf("\n\nTHE GUESS IS\n\n....");
+    printf("INCORRECT!\n");
+    computerScore++;
+
+}
+
+void getPlayerGuess(){
+
+    printf("\n\nPLAYER, GUESS IF THE NUMBER IS EVEN OR ODD\n\n");
+
+}
+
+
+int getComputerGuess(){
+
+    int guess;
+    while(1){
+        int theNumber = generateRandomNumber();
+        if(theNumber % 2 == 0){
+            guess = theNumber;
+            break;
+        }
+    }
+    return guess;
+
+}
