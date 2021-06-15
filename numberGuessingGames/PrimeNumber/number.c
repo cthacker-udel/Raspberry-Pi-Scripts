8a2fe3a72cb10f842ccdcf523a552b5014e77837
cthacker-udel <cthacker@udel.edu>
2021-06-15 15:35:30 -0400

    Added implementation to generate random number

---
+++ numberGuessingGames/PrimeNumber/number.c
@@ -0,0 +1,13 @@
+
+int getRandomNumber(){
+	return rand();
+}
+
+void getPlayerGuess(){
+	printf("\nPLAYER : GUESS IF THE NUMBER IS PRIME(y/n)?\n");
+}
+
+void printComputerGuess(){
+	printf("\nCOMPUTER GUESSED NUMBER\n");
+}
+
---
+++ numberGuessingGames/PrimeNumber/project.h
@@ -0,0 +1,5 @@
+int getRandomNumber();
+
+void getPlayerGuess();
+
+void printComputerGuess();
