eb6c4257d4cebfdd13a67081089d2b01f5249a2a
cthacker-udel <cthacker@udel.edu>
2021-06-14 21:24:22 -0400

    Implemented Decagon class

---
+++ cpp-practice/Project-Shapes/Decagon.hpp
@@ -0,0 +1,21 @@
+class Decagon{
+
+    int side;
+    int area;
+    int perimeter;
+
+    public:
+
+        Decagon();
+
+        Decagon(int);
+
+        int getArea();
+
+        int getPerimeter();
+
+        int getSide();
+
+        void setSide(int);
+
+}
