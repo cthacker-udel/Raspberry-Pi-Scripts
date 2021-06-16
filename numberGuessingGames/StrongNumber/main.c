995483a83906de46312182d8c80ff0535671cff1
cthacker-udel <cthacker@udel.edu>
2021-06-16 13:07:21 -0400

    Added implementation for correct guesses and incorrect guesses by the user

--- numberGuessingGames/StrongNumber/main.c
+++ numberGuessingGames/StrongNumber/main.c
@@ -9,6 +9,20 @@ int main(int argc, char *argv[]){
 		seedRandomNumber(7);
 	}
 	while(1){
-		
+		int number = generateRandomNumber();
+		printf("\nCOMPUTER GENERATED NUMBER\n");
+		int answer = getUserInput();
+		if(isStrongNumber(number) & answer == 1){
+			// correct guess
+		}
+		else if(isStrongNumber(number) && answer == 0){
+			// incorrect guess
+		}
+		else if(!isStrongNumber(number) && answer == 0){
+			// correct guess
+		}
+		else if(!isStrongNumber(number) && answer == 1){
+			// incorrect guess
+		}		
 	}
 }
