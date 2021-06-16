6b89e139ed79185b7f6827cdfbf127ec57ad5e7d
cthacker-udel <cthacker@udel.edu>
2021-06-16 12:55:30 -0400

    Added base functions in number.c

--- numberGuessingGames/StrongNumber/number.c
+++ numberGuessingGames/StrongNumber/number.c
@@ -1,1 +1,25 @@
 #include "project.h"
+
+void seedRandomNumber(int number){
+	srand(number);
+}
+
+int generateRandomNumber(){
+	return rand();
+}
+
+int isNumberStrong(int number){
+	int sum = 0;
+	while(number > 10){
+
+	}
+}
+
+int factorial(int number){
+	if(number < 10){
+		return number;
+	}
+	else{
+		return number % 10 * factorial(number / 10);
+	}
+}
