58abfc35e616b03435ae66ad59165a53fc2b180b
cthacker-udel <cthacker@udel.edu>
2021-06-16 13:01:16 -0400

    Added implementation to seed random number with argc and argv

--- numberGuessingGames/StrongNumber/main.c
+++ numberGuessingGames/StrongNumber/main.c
@@ -1,1 +1,15 @@
 #include "project.h"
+
+int main(int argc, char *argv[]){
+	if(argc > 1){
+		int number = atol(argv[1]);
+		seedRandomNumber(number);
+	}
+	else{
+		seedRandomNumber(7);
+	}
+	while(1){
+
+
+	}
+}
