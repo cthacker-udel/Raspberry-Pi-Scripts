43d618f4011db6e3fbc3389504f8ee0b442be654
cthacker-udel <cthacker@udel.edu>
2021-06-20 06:24:43 -0400

    Added Player class for EvenOrOdd game in C++

---
+++ cpp-practice/Project-EvenOrOdd/Player.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+class Player{
+
+    string name;
+    int points;
+
+    public:
+
+        Player();
+
+        Player(string);
+
+
+};
