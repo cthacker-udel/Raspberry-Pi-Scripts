7db60163ffc4192a1042b620faacc4611e5857ea
cthacker-udel <cthacker@udel.edu>
2021-06-20 06:32:03 -0400

    Added Game.hpp file

---
+++ cpp-practice/Project-EvenOrOdd/Game.hpp
@@ -0,0 +1,20 @@
+#include "Player.hpp"
+
+
+class Game{
+
+    Player player1;
+    Player player2;
+
+
+    public:
+
+        Game();
+
+        void setPlayer1(Player player1);
+
+        void setPlayer2(Player player2);
+
+        void startGame();
+
+};
