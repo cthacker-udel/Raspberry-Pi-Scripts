6715fcda0342c205970fde329249356ae5dc1e1b
cthacker-udel <cthacker@udel.edu>
2021-06-23 07:47:46 -0400

    Added base Board class for Two Dimensional Pointer Arithmetic

---
+++ cpp-practice/PointerArithmetic/Pointer-TwoDimensional/Board.hpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+class Board{
+
+    int *board;
+
+    public:
+
+        Board();
+
+        int *getBoard;
+
+        void setBoard(int *);
+
+
+};
