53099a197e53afbbf7c2a5ddeb2090a9d28ca9a3
cthacker-udel <cthacker@udel.edu>
2021-06-24 05:19:10 -0400

    Added implementation in numberMethods.java class to get random even number, odd number, and prime number, and also added the isPrime method

--- javaPractice/ProjectRandomNumbers/EvenNumbers/numberMethods.java
+++ javaPractice/ProjectRandomNumbers/EvenNumbers/numberMethods.java
@@ -1,12 +1,62 @@
 package javaPractice.ProjectRandomNumbers.EvenNumbers;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class numberMethods {
 
     public int getRandomNumber(){
 
-        Random random = new Random();
+        SecureRandom random = new SecureRandom();
+        random.setSeed(System.currentTimeMillis());
         return random.ints().parallel().findFirst().getAsInt();
+
+    }
+
+    public int getRandomEvenNumber(){
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(System.currentTimeMillis());
+        return secureRandom.ints().parallel().filter(e -> e % 2 == 0).findFirst().getAsInt();
+
+    }
+
+    public int getRandomOddNumber(){
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(System.currentTimeMillis());
+        return secureRandom.ints().parallel().filter(e -> e % 2 != 0).findFirst().getAsInt();
+
+    }
+
+    public boolean isPrime(int number){
+
+        if(number < 2 && number >= 0){
+            return true;
+        }
+        else if(number == 2 || number == 3 || number == 5){
+            return true;
+        }
+        else if(number % 2 == 0 || number % 3 == 0 || number % 5 == 0){
+            return false;
+        }
+        else{
+
+            for(int i = 2; i < (int)Math.sqrt(number)+1; i++){
+                if(number % i == 0){
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+    }
+
+    public int getRandomPrimeNumber(){
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(System.currentTimeMillis());
+        return secureRandom.ints().parallel().filter(e -> isPrime(e)).findFirst().getAsInt();
+
 
     }
 
