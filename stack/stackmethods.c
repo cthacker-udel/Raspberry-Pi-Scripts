2dd82409ac3e5d0bbcefc8afeaa8fd0839b53073
cthacker-udel <cthacker@udel.edu>
2021-06-24 00:08:54 -0400

    Implemented createNode function

--- stack/stackmethods.c
+++ stack/stackmethods.c
@@ -2,6 +2,13 @@
 
 node *HEAD = NULL;
 
+node *createNode(int newValue){
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->value = newValue;
+    newNode->next = NULL;
+    return newNode;
+}
+
 int push(int newValue){
     if(HEAD == NULL){
         // 1 line
