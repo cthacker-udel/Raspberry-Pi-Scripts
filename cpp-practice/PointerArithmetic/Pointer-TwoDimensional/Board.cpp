e6dca4a5441f1867f272b5d077c95368e2170a5c
cthacker-udel <cthacker@udel.edu>
2021-06-24 04:36:42 -0400

    Implemented oddCount functionality

--- cpp-practice/PointerArithmetic/Pointer-TwoDimensional/Board.cpp
+++ cpp-practice/PointerArithmetic/Pointer-TwoDimensional/Board.cpp
@@ -57,6 +57,26 @@ int Board::evenCount(){
 
 }
 
+int Board::oddCount(){
+
+    int count = 0;
+
+    for(int i = 0; i < size; i++){
+
+        for(int j = 0; j < size; j++){
+
+            int value = *(*(board+i)+j);
+            if(value % 2 != 0){
+                count++;
+            }
+
+        }
+
+    }
+    return count;
+
+}
+
 
 void Board::craftBoard(){
 
