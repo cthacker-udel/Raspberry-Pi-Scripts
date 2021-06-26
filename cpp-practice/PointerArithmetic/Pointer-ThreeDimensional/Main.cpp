edd998ab96b8d01e8e9980bf620d8d0d17d57b02
cthacker-udel <cthacker@udel.edu>
2021-06-26 12:45:32 -0400

    Added method to collect sum, avg, evencount, oddcount, and primecount of three-dimensional integer pointer array

--- cpp-practice/PointerArithmetic/Pointer-ThreeDimensional/Main.cpp
+++ cpp-practice/PointerArithmetic/Pointer-ThreeDimensional/Main.cpp
@@ -2,8 +2,16 @@
 
 int main(void){
 
-    Matrix matrix(4,5,4);
+    Matrix matrix(3,3,3);
     matrix.createMatrix();
     matrix.printMatrix();
+    int sum = matrix.sumMatrix();
+    int avg = matrix.avgMatrix();
+    int sub = matrix.subMatrix();
+    int evenCount = matrix.evenCount();
+    int oddCount = matrix.oddCount();
+    int primeCount = matrix.primeCount();
+
+    cout << "\nSUM : " << sum << "\nAVG : " << avg << "\nSUB : " << sub << "\nEVENCOUNT : " << evenCount << "\nODDCOUNT : " << oddCount << "\nPRIMECOUNT : " << primeCount << endl;
 
 }
