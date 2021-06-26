cdf45a0830d43ff19ae73210cebd9aafb9470a66
cthacker-udel <cthacker@udel.edu>
2021-06-26 02:53:58 -0400

    Added base methods to implement in project.h

---
+++ library/project.h
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+typedef struct eachBook{
+
+    int ISBN;
+    char *name;
+    int pages;
+    int chapters;
+    char *author;
+    int fiveStarReviews;
+    int fourStarReviews;
+    int threeStarReviews;
+    int twoStarReviews;
+    int oneStarReviews;
+    struct eachBook *next;
+
+}book;
+
+typedef struct theLibrary{
+
+    char *name;
+    int founded;
+    char *founder;
+    int numBooks;
+    book *books;
+
+}library;
+
+
+int getNumBooks(void);
+
+book *createBook();
+
+void addBook(book *);
+
+void addISBN(book *);
+
+void addName(book *);
+
+void addPages(book *);
+
+void addChapters(book *);
+
+void addAuthor(book *);
+
+void addFiveStarReview(book *);
+
+void addFourStarReview(book *);
+
+void addThreeStarReview(book *);
+
+void addTwoStarReview(book *);
+
+void addOneStarReview(book *);
+
+void sortByISBN(book *);
+
+void sortByName(book *);
+
+void sortByPages(book *);
+
+void sortByChapters(book *);
+
+void sortByAuthor(book *);
+
+void sortByNumFiveStarReviews(book *);
+
+void sortByNumFourStarReviews(book *);
+
+void sortByNumThreeStarReviews(book *);
+
+void sortByNumTwoStarReivews(book *);
+
+void sortByNumOneStarReviews(book *);
