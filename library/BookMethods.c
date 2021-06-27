79d21dc62ce055289f828c8ab86db3c287d22e8b
cthacker-udel <cthacker@udel.edu>
2021-06-27 17:09:27 -0400

    Added sortByChapters method in BookMethods.c

--- library/BookMethods.c
+++ library/BookMethods.c
@@ -188,6 +188,88 @@ void sortByISBNDescending(){
 
 }
 
+void sortByChaptersAscending(){
+
+    int loopvar = 1;
+    while(1){
+        book *tempHead = HEAD;
+        book *prevNode = HEAD;
+        book *forwardNode;
+        while(prevNode != NULL && forwardNode != NULL){
+            forwardNode = prevNode->next;
+            if(forwardNode != NULL && prevNode->chapters > forwardNode->chapters){
+                prevNode->next = forwardNode->next;
+                forwardNode->next = prevNode;
+                if(prevNode == HEAD){
+                    HEAD = forwardNode;
+                    loopvar = 0;
+                }
+                else{
+                    HEAD = tempHead;
+                    book *temp = HEAD;
+                    while(temp->next != NULL){
+                        if(temp->next == prevNode){
+                            temp->next = forwardNode;
+                            break;
+                        }
+                    }
+                    loopvar = 0;
+                }
+                break;
+            }
+            loopvar = 1;
+            prevNode = prevNode->next;
+        }
+        if(loopvar){
+            HEAD = tempHead;
+            break;
+        }
+    }
+
+}
+
+
+void sortByChaptersDescending(){
+
+    int loopvar = 1;
+    while(1){
+        book *tempHead = HEAD;
+        book *prevNode = HEAD;
+        book *forwardNode;
+        while(prevNode != NULL && forwardNode != NULL){
+            forwardNode = prevNode->next;
+            if(forwardNode != NULL && prevNode->chapters < forwardNode->chapters){
+                prevNode->next = forwardNode->next;
+                forwardNode->next = prevNode;
+                if(prevNode == HEAD){
+                    HEAD = forwardNode;
+                    loopvar = 0;
+                }
+                else{
+                    HEAD = tempHead;
+                    book *temp = HEAD;
+                    while(temp->next != NULL){
+                        if(temp->next == prevNode){
+                            temp->next = forwardNode;
+                            break;
+                        }
+                    }
+                    loopvar = 0;
+                }
+                break;
+            }
+            loopvar = 1;
+            prevNode = prevNode->next;
+        }
+        if(loopvar){
+            HEAD = tempHead;
+            break;
+        }
+    }
+
+}
+
+
 void sortByNameAscending(){
 
     int loopvar = 1;
