99b464e0b45f38dc96a9384a589e2c949249920d
cthacker-udel <cthacker@udel.edu>
2021-06-27 16:01:00 -0400

    Added switch implementation

--- library/main.c
+++ library/main.c
@@ -4,11 +4,54 @@ int main(void){
 
 
     library *theLibrary = getLibrary();
-    book *books;
+    book *books = getHead();
+    book *newBook;
+    int choice = 0;
     while(1){
 
         printMenu();
-
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                newBook = createBook();
+                break;
+            case 2:
+                addBook(newBook);
+                break;
+            case 3:
+                addISBN(newBook);
+                break;
+            case 4:
+                addName(newBook);
+                break;
+            case 5:
+                addPages(newBook);
+                break;
+            case 6:
+                addChapters(newBook);
+                break;
+            case 7:
+                addAuthor(newBook);
+                break;
+            case 8:
+                addFiveStarReview(newBook);
+                break;
+            case 9:
+                addFourStarReview(newBook);
+                break;
+            case 10:
+                addThreeStarReview(newBook);
+                break;
+            case 11:
+                addTwoStarReview(newBook);
+                break;
+            case 12:
+                addOneStarReview(newBook);
+                break;
+            case 13:
+                sortByISBNAscending();
+        }
+        
 
     }
 
