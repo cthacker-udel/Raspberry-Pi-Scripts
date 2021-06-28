c72600e3bb2bf2b9a061a09c6c6292b67c06807c
cthacker-udel <cthacker@udel.edu>
2021-06-28 16:17:38 -0400

    Added Node implementation for DoublyLinkedList

---
+++ cpp-practice/Project-LinkedList/DoublyLinkedList/Node.hpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Node{
+
+    int value;
+    Node *next;
+    Node *previous;
+
+    public:
+
+        Node();
+
+        Node(int);
+
+
+
+};
