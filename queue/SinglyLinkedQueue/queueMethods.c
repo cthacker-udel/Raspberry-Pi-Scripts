0b72bfb278c395a51ec40b7da3a837f277a0e3ec
cthacker-udel <cthacker@udel.edu>
2021-06-29 06:15:30 -0400

    Added size function in queueMethods.c ;

--- queue/SinglyLinkedQueue/queueMethods.c
+++ queue/SinglyLinkedQueue/queueMethods.c
@@ -138,3 +138,13 @@ int removeAll(){
         return 1;
     }
 }
+
+int size(){
+    int count = 0;
+    node *tempHead = HEAD;
+    while(tempHead != NULL){
+        tempHead = tempHead->next;
+        count++;
+    }
+    return count;
+}
