7ed32ee00467a5d049346d4802b60d68daf91405
cthacker-udel <cthacker@udel.edu>
2021-06-29 05:16:23 -0400

    Added base classes for Stack CPP project

---
+++ cpp-practice/Project-Stack/Node.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+
+class Node{
+
+    int value;
+    Node *next;
+
+    public:
+
+        Node();
+
+        Node(int);
+
+
+}
---
+++ cpp-practice/Project-Stack/Stack.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include "Node.hpp"
+using namespace std;
+
+class Stack{
+
+    Node *head;
+
+
+
+
+};
