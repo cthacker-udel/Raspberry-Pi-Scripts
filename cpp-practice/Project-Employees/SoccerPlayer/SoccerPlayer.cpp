be37342a9cebbe689e7c881f4ae992b6e1a8e311
cthacker-udel <cthacker@udel.edu>
2021-06-30 15:51:24 -0400

    Added no-arg constructor of soccer player to SoccerPlayer.cpp

---
+++ cpp-practice/Project-Employees/SoccerPlayer/SoccerPlayer.cpp
@@ -0,0 +1,14 @@
+#include "SoccerPlayer.hpp"
+
+SoccerPlayer::SoccerPlayer(){
+
+    name = "Ronaldo";
+    age = 40;
+    goals = 1000;
+    shotsAtt = 2000;
+    yellowCards = 40;
+    redCards = 20;
+    passComplete = 10000;
+    passAtt = 20000;
+
+}
---
+++ cpp-practice/Project-Employees/SoccerPlayer/SoccerPlayer.hpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class SoccerPlayer{
+
+    string name;
+    int age;
+    int goals;
+    int shotsAtt;
+    int yellowCards;
+    int redCards;
+    int passComplete;
+    int passAtt;
+
+    public:
+
+        SoccerPlayer();
+
+        SoccerPlayer(string);
+
+        string getName();
+
+        int getAge();
+
+        void setAge(int);
+
+        int getGoals();
+
+        void setGoals(int);
+
+        int getShotsAtt();
+
+        void setShotsAtt(int);
+
+        int getYellowCards();
+
+        void setYellowCards(int);
+
+        int getRedCards();
+
+        void setRedCards(int);
+
+        int getPassComplete();
+
+        void setPassComplete(int);
+
+        int getPassAtt();
+
+        void setPassAtt(int);
+
+
+};
