100464fc51178d2530663af7f57de60fbcb30bc6
cthacker-udel <cthacker@udel.edu>
2021-06-30 06:49:27 -0400

    Added prototypes to football player.hpp

---
+++ cpp-practice/Project-Employees/FootballPlayer/FootballPlayer.hpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+class FootballPlayer{
+
+    string name;
+    int age;
+    int yardsThrown;
+    int interceptionsThrown;
+    int completions;
+    int attmepts;
+    int runYardsRan;
+
+    public:
+
+        FootballPlayer();
+
+        FootballPlayer(string);
+
+        int getAge();
+
+        string getName();
+
+        void setName(string);
+
+        int getYardsThrown();
+
+        void setYardsThrown(int);
+
+        int getInterceptionsThrown();
+
+        void setInterceptionsThrown(int);
+
+        int getCompletions();
+
+        void setCompletions(int);
+
+        int getAttempts();
+
+        void setAttempts(int);
+
+        int getRunYards();
+
+        void setRunYards(int);
+};
