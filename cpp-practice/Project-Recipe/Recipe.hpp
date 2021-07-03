069eca2057092176eac580bfb11a48b324e47568
cthacker-udel <cthacker@udel.edu>
2021-07-03 05:53:59 -0400

    Added prototypes to Recipe.hpp

--- cpp-practice/Project-Recipe/Recipe.hpp
+++ cpp-practice/Project-Recipe/Recipe.hpp
@@ -5,10 +5,24 @@
 #include <stdlib.h>
 #include <math.h>
 #include "Ingredient.hpp"
+#include "Step.hpp"
 
 class Recipe{
 
     vector<Ingredient> ingredients;
-    vector<String> 
+    vector<Step> steps;
+    string recipeName;
+
+    public:
+
+        Recipe(string);
+
+        vector<Ingredient> getIngredients();
+
+        vector<Step> getSteps;
+
+        void addIngredient(Ingredient);
+
+        void addStep(Step);
 
 }
