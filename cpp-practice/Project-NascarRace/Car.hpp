c52a28010a96d3ba75e34c640eaa7af903d60fdb
cthacker-udel <cthacker@udel.edu>
2021-07-04 07:49:11 -0400

    Added Car.hpp implementation

--- cpp-practice/Project-NascarRace/Car.hpp
+++ cpp-practice/Project-NascarRace/Car.hpp
@@ -1,10 +1,33 @@
 #include <string>
 #include <cmath>
 #include <iostream>
+#include "Driver.hpp"
 using namespace std;
 
 class Car{
 
+    Driver theDriver;
+    int hp;
+    int mph;
+    string model;
+    int milesDriven;
 
+    public:
+
+        Car(Driver,string,int,int);
+
+        Car(string,int,int);
+
+        void setDriver(Driver);
+
+        int getHP();
+
+        int getMPH();
+
+        string getModel();
+
+        int getMilesDriven();
+
+        void setMilesDriven(int);
 
 };
