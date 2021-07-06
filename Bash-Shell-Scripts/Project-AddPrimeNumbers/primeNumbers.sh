656cc60f13d46594262140f9910de2f0b950db2e
cthacker-udel <cthacker@udel.edu>
2021-07-06 01:10:39 -0400

    Modified all three bash scripts

--- Bash-Shell-Scripts/Project-AddPrimeNumbers/primeNumbers.sh
+++ Bash-Shell-Scripts/Project-AddPrimeNumbers/primeNumbers.sh
@@ -8,11 +8,11 @@ is_prime(){
 
     number=$1
     start_number=2
-    while [ start_number -lt number ]; do
+    while [ $start_number -lt $number ]; do
         if [ $((number % start_number)) -eq 0 ]; then
-            echo "Not a prime number"
             return 0
         fi
+        ((start_number++))
     done
     return 1
 
@@ -21,13 +21,11 @@ is_prime(){
 read -p "How many prime numbers do you want to generate?" num
 
 
-while [ count -lt ${num} ]; do
-    echo -e "\nGenerating first prime number\n"
-    number=$(shuf -i 1-10000 -n 1)
-    if [ is_prime $number -eq 1 ]; then
+while [ $count -lt ${num} ]; do
+    number=$(shuf -i 1-100000 -n 1)
+    is_prime $number
+    if [ $? -eq 1 ]; then
         echo -e "\nThe number ${number} is prime"
         ((count++))
-    else
-        echo -e "\nThe number ${number} is not prime\n"
     fi
 done
