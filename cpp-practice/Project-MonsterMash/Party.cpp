23021f40aa372e0045b462da87981074566dafb8
cthacker-udel <cthacker@udel.edu>
2021-07-08 04:45:40 -0400

    added check_party_size method

--- cpp-practice/Project-MonsterMash/Party.cpp
+++ cpp-practice/Project-MonsterMash/Party.cpp
@@ -26,5 +26,20 @@ bool Party::were_only_werewolves(Party theParty){
 
 }
 
+string Party::check_party_size(Party theParty){
+
+    int numGuests = sumGuests();
+    if(numGuests <= 20){
+        return "small";
+    }
+    else if(numGuests > 20 && numGuests < 40){
+        return "medium";
+    }
+    else{
+        return "big";
+    }
+
+}
+
 
 
