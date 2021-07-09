26c8b5b09ce43bfe2a0b8ff5e71bcd9ebc4f067d
cthacker-udel <cthacker@udel.edu>
2021-07-08 21:32:41 -0400

    added find_most_calorific_ratio

--- cpp-practice/Project-MonsterMash/Treat.cpp
+++ cpp-practice/Project-MonsterMash/Treat.cpp
@@ -19,3 +19,13 @@ int Treat::eat_candy(vector<Treat> treats){
 
 }
 
+float Treat::find_most_calorific_ratio(vector<Treat> treats){
+
+    float max = 0.0;
+    for(int i = 0; i < treats.size(); i++){
+        Treat theTreat = treats.at(i);
+        max = fmax(max,theTreat.calories / theTreat.quantity);
+    }
+    return max;
+
+}
