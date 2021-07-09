ea356f6b1259a9197b06bbfb9d50f329460b4943
cthacker-udel <cthacker@udel.edu>
2021-07-08 22:17:00 -0400

    added potion and ingredient classes

---
+++ cpp-practice/Project-MonsterMash/Ingredient.hpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Ingredient{
+
+    string name;
+    bool rare;
+
+    public:
+
+        Ingredient(string,bool);
+
+
+};
---
+++ cpp-practice/Project-MonsterMash/Potion.hpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include "Ingredient.hpp"
+using namespace std;
+
+class Potion{
+
+    string effect;
+    vector<Ingredient> ingredients;
+    int timeRequired;
+
+    public:
+
+        Potion(string,vector<Ingredient>,int);
+
+        
+
+
+
+
+};
