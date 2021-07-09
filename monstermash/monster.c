285d499d9bbf891a72005d7637f46f51c59188b0
cthacker-udel <cthacker@udel.edu>
2021-07-09 02:47:29 -0400

    added method average_spookiness

--- monstermash/monster.c
+++ monstermash/monster.c
@@ -21,3 +21,24 @@ int count_undead_monsters(monster *theMonsters, int len){
 
 
 }
+
+float average_spookiness(monster *theMonsters, int len){
+
+    if(len == 0){
+        return 0.0;
+    }
+    else{
+
+        int total = 0;
+        for(int i = 0; i < len; i++){
+
+            monster theMonster = *(theMonsters+i);
+            total += theMonster.spookyiness;
+
+        }
+        return total;
+
+    }
+
+
+}
--- monstermash/monster.h
+++ monstermash/monster.h
@@ -13,7 +13,7 @@ int count_monsters(monster *,int);
 
 int count_undead_monsters(monster *,int);
 
-float average_spookiness(monster[],int);
+float average_spookiness(monster *,int);
 
 float average_undead_spookiness(monster[],int);
 
