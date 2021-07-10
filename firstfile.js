f927422e7c40ebb3d0ff5156b87ce34d2a961600
cthacker-udel <cthacker@udel.edu>
2021-07-10 04:44:53 -0400

    first js commit

---
+++ firstfile.js
@@ -0,0 +1,153 @@
+let square_string = "#";
+for(let i = 1; i < 8; i++){
+    console.log(square_string);
+    square_string += "#";
+}
+
+for(let i = 1; i < 101; i++){
+
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz" + String(i));
+    }
+    else if(i % 3 === 0){
+
+        console.log("Fizz" + String(i));
+
+    }
+    else if(i % 5 === 0){
+
+        console.log("Buzz" + String(i));
+
+    }
+
+}
+
+let chess_board = "";
+
+let size = 100;
+
+let odd = true;
+let even = false;
+
+for(let i = 0; i < size; i++){
+
+    for(let j = 0; j < size; j++){
+
+        if(odd){
+            chess_board += " ";
+        }
+        if(odd && i % 2 === 0){
+            chess_board += "#";
+        }
+        if(even){
+            chess_board += "#";
+        }
+        if(even && i % 2 !== 0){
+            chess_board += " ";
+        }
+
+    }
+    if(odd){
+        even = true;
+        odd = false;
+        chess_board += "\n";
+    }
+    else if(even){
+        even = false;
+        odd = true;
+        chess_board += "\n";
+    }
+
+}
+console.log(chess_board);
+
+const square = function(x){
+
+    return x*x;
+
+}
+
+const makeNoise = function(){
+
+    console.log("NOISE");
+
+}
+
+makeNoise();
+
+const power = function(base,exponent){
+
+    return base ** exponent;
+
+};
+
+console.log(power(2,4));
+
+const hummus = function(factor){
+
+    const ingredient = function(amount,unit,name){
+
+        let ingredientAmount = amount * factor;
+        if(ingredientAmount > 1){
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit} ${name}`);
+
+
+    };
+    ingredient(1,"can","chickpeas");
+
+
+
+};
+
+function square_two(value){
+
+    return value*value;
+
+}
+
+hummus(10);
+
+console.log(square_two(20));
+
+
+const power_two = (base,exponent) => {
+
+    return base ** exponent;
+
+};
+
+console.log(power_two(10,1));
+
+
+const minus = (x,y) => {
+
+    if(y === undefined){
+        return -x;
+    }
+    else{
+
+        return x - y;
+
+    }
+
+};
+
+console.log(minus(10,6));
+
+const default_values = (the_default="defaultval",two="twovals") => {
+
+    if(the_default === "defaultval"){
+        console.log("no defaultval given");
+    }
+    else if(two === "twovals"){
+        console.log("twovals value was not given");
+    }
+    else{
+        console.log("reached else");
+    }
+
+};
+
+default_values("one val","tt");
