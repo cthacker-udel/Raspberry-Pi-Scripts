19b3c715c3c9e818e814a08193825ef8fbbd9881
cthacker-udel <cthacker@udel.edu>
2021-07-10 11:23:37 -0400

    added grave class

--- monstermash/grave.c
+++ monstermash/grave.c
@@ -14,5 +14,36 @@ int count_grave_all(grave *graves, int len){
     }
     return total;
 
+}
+
+
+int count_grave_characters(grave *graves, int len){
+
+    int total = 0;
+    for(int i = 0; i < len; i++){
+
+        grave theGrave = *(graves+i);
+        int graveMessageLen = theGrave.message;
+        int graveNameLen = theGrave.name;
+        for(int j = 0; j < graveMessageLen; j++){
+
+            char graveMessageChar = *(theGrave.message+j);
+            if(isalpha(graveMessageChar)){
+                total++;
+            }
+
+        }
+        for(int j = 0; j < graveNameLen; j++){
+
+            char graveNameChar = *(theGrave.name);
+            if(isalpha(graveNameChar)){
+                total++;
+            }
+
+        }
+
+    }
+    return total;
+
 
 }
--- monstermash/grave.h
+++ monstermash/grave.h
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <ctype.h>
+
 typedef struct theGrave{
 
     char *name;
