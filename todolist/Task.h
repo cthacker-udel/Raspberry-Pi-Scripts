b029ce482e5d3d766ac590b86ae07f4e9783c97a
cthacker-udel <cthacker@udel.edu>
2021-07-09 22:53:56 -0400

    added files to todolist c implementation

---
+++ todolist/Task.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct theTask{
+
+    char *name;
+    int id;
+    int rank;
+    int hr;
+    int min;
+
+}task;
+
+
+void printTask();
