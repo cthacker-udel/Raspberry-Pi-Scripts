7ff8ab822e8c104ce426a3bcae78420a973d5848
cthacker-udel <cthacker@udel.edu>
2021-07-11 23:27:30 -0400

    added User.cpp implementation

--- cpp-practice/Project-CarRentalSystem/User.cpp
+++ cpp-practice/Project-CarRentalSystem/User.cpp
@@ -2,8 +2,23 @@
 
 User::User(){
 
+    string newFirst;
+    string newLast;
+    int bYear;
+    int bMonth;
+    int bDay;
 
-    
+    cout << "Enter your first name" << endl;
+    cin >> this->firstName;
+    cout << "Enter your last name" << endl;
+    cin >> this->lastName;
+    cout << "Enter your birth year" << endl;
+    cin >> this->birthYear;
+    cout << "Enter your birth month" << endl;
+    cin >> this->birthMonth;
+    cout << "Enter your birth day" << endl;
+    cin >> this->birthDay;
+
 }
 
 User::User(string newFirstName, string newLastName, int newBirthYear, int newBirthMonth, int newBirthDay){
