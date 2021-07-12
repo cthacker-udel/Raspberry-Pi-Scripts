cfcc5b698157eb94fd4474e6ea908f928dc58b42
cthacker-udel <cthacker@udel.edu>
2021-07-12 01:21:10 -0400

    added method eat_candy

--- monstermash/treat.c
+++ monstermash/treat.c
@@ -1,1 +1,12 @@
 #include "treat.h"
+
+int eat_candy(treat *treats, int len){
+
+    int total = 0;
+    for(int i = 0; i < len; i++){
+        treat theTreat = *(treats+i);
+        total += theTreat.calories;
+    }
+    return total;
+
+}
