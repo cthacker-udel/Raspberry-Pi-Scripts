4a5352a6c7fa4c13400f1d62d0ec3317fd8adbfa
cthacker-udel <cthacker@udel.edu>
2021-07-12 01:38:17 -0400

    added take_until_long method

--- monstermash/media.c
+++ monstermash/media.c
@@ -23,3 +23,17 @@ int count_not_long(media *medias, int len){
     return count;
 
 }
+
+int take_until_long(media *medias, int len){
+
+    int count = 0;
+    for(int i = 0; i < len; i++){
+        media bMedia = *(medias+i);
+        if(bMedia.duration >= 100){
+            return count;
+        }
+        count++;
+    }
+    return count;
+
+}
