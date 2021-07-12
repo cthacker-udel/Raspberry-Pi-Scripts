8cfa52d609776d60ecf14a2849aa5feae2f1d0ca
cthacker-udel <cthacker@udel.edu>
2021-07-12 01:22:56 -0400

    added find_most_calorific_ratio

--- monstermash/treat.c
+++ monstermash/treat.c
@@ -10,3 +10,15 @@ int eat_candy(treat *treats, int len){
     return total;
 
 }
+
+float find_most_calorific_ratio(treat *treats, int len){
+
+    float max = 0.0;
+    for(int i = 0; i < len; i++){
+        treat theTreat = *(treats+i);
+        float ratio = theTreat.calories*1.0 / theTreat.quantity;
+        max = fmax(max,ratio);
+    }
+    return max;
+
+}
--- monstermash/treat.h
+++ monstermash/treat.h
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef struct theTreat{
 
