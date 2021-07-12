967e52c1da0306baa70364789aaf220b2c065244
cthacker-udel <cthacker@udel.edu>
2021-07-11 23:18:48 -0400

    added User.cpp implementation

---
+++ cpp-practice/Project-CarRentalSystem/User.cpp
@@ -0,0 +1,11 @@
+#include "User.hpp"
+
+User::User(string newFirstName, string newLastName, int newBirthYear, int newBirthMonth, int newBirthDay){
+
+    this->firstName = newFirstName;
+    this->lastName = newLastName;
+    this->birthYear = newBirthYear;
+    this->birthMonth = newBirthMonth;
+    this->birthDay = newBirthDay;
+
+}
