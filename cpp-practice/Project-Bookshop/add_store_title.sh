d7f1c00b40a1e8b1779301ebd099a45ea1371d5f
cthacker-udel <cthacker@udel.edu>
2021-07-15 04:47:36 -0400

    added implementation to add_store_title.sh

--- cpp-practice/Project-Bookshop/add_store_title.sh
+++ cpp-practice/Project-Bookshop/add_store_title.sh
@@ -2,6 +2,13 @@
 
 if [ ! -e bookstore_records.txt ]; then
     touch bookstore_records.txt
+    exit 0
 else
     echo "Bookstore records have already been instantiated"
 fi
+
+name=$1
+
+echo "-=-=-=-=-=-=$name-=-=-=-=-=-=" >> bookstore_records.txt
+
+echo "Title has been added to bookstore_records.txt file"
--- cpp-practice/Project-Bookshop/main.cpp
+++ cpp-practice/Project-Bookshop/main.cpp
@@ -1,8 +1,13 @@
 #include "Store.hpp"
 
+Store theStore = NULL;
+
 int main(void){
 
-    
+    if(theStore == NULL){
+        system("./add_store_title.sh")
+        
+    }
 
 
 
