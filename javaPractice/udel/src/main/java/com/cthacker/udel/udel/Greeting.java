3546097b7208262c1bfd2a936e9eadf2e37fc36c
cthacker-udel <cthacker@udel.edu>
2021-07-21 05:01:22 -0400

    update

---
+++ javaPractice/udel/src/main/java/com/cthacker/udel/udel/Greeting.java
@@ -0,0 +1,23 @@
+package com.cthacker.udel.udel;
+
+public class Greeting{
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+
+        this.id = id;
+        tihs.content = content;
+
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+}
--- javaPractice/udel/src/main/java/com/cthacker/udel/udel/UdelApplication.java
+++ javaPractice/udel/src/main/java/com/cthacker/udel/udel/UdelApplication.java
@@ -2,6 +2,8 @@ package com.cthacker.udel.udel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 
 @SpringBootApplication
 public class UdelApplication {
