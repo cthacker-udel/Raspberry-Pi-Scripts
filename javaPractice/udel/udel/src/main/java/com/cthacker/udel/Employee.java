4eef81d6a6256b06c0a4aa0fb4585098bfbe52bd
cthacker-udel <cthacker@udel.edu>
2021-07-23 02:47:42 -0400

    update interface

---
+++ javaPractice/udel/udel/src/main/java/com/cthacker/udel/Employee.java
@@ -0,0 +1,15 @@
+package udel.src.main.java.com.cthacker.udel;
+
+import org.springframework.data.annotation.Id;
+
+public class Employee {
+
+    @Id private String employeeId;
+
+    private String firstName;
+    private String lastName;
+    private String position;
+    private 
+
+    
+}
