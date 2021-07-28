45dec5a856094a6b12165a2435183127c4189871
cthacker-udel <cthacker@udel.edu>
2021-07-28 04:48:25 -0400

    Added implementation for Board no-arg constructor

--- cpp-practice/Project-Connect4/Board.cpp
+++ cpp-practice/Project-Connect4/Board.cpp
@@ -12,18 +12,88 @@ Board::Board(){
     cin >> answer;
     switch(answer){
 
-        case 2:
-            break;
+        case 2:{
 
-        case 1:
-            break;
+            string player1Name = "";
+            char player1Piece = ' ';
 
-        case 0:
-            break;
+            do{
 
-        default:
+                cout << "Enter Player1's Name" << endl;
+                cin >> player1Name;
+
+            }while(player1Name.length() == 0);
+
+            do{
+
+                cout << "Enter Player1's Piece" << endl;
+                cin >> player1Piece;
+
+            }while(player1Piece == ' ');
+
+
+            string player2Name = "";
+            char player2Piece = ' ';
+
+            do{
+
+                cout << "Enter Player2's Name" << endl;
+                cin >> player2Name;
+
+            }while(player2Name.length() == 0);
+
+            do{
+
+                cout << "Enter Player2's Piece" << endl;
+                cin >> player2Piece;
+
+            }while(player2Piece == ' ');
+
+            this->player1 = new Player(player1Name,player1Piece);
+            this->player2 = new Player(player2Name,player2Piece);
+
+            break;
+        }
+
+        case 1:{
+
+            string player1Name = "";
+            char player1Piece = ' ';
+
+            do{
+
+                cout << "Enter Player1's Name" << endl;
+                cin >> player1Name;
+
+            }while(player1Name.length() == 0);
+
+            do{
+
+                cout << "Enter Player1's Piece" << endl;
+                cin >> player1Piece;
+
+            }while(player1Piece == ' ');
+
+            this->player1 = new Player(player1Name,player1Piece);
+
+
+            this->player2 = new Player("computer2",'C');
+
+            break;
+        }
+
+        case 0:{
+
+            this->player1 = new Player("computer1",'X');
+            this->player2 = new Player("computer2",'O');
+
+            break;
+        }
+
+        default:{
             cout << "Malformed input" << endl;
             break;
+        }
 
     }
 
--- cpp-practice/Project-Connect4/Board.hpp
+++ cpp-practice/Project-Connect4/Board.hpp
@@ -4,8 +4,8 @@ class Board{
 
     int size = 8;
     char **board = new char[size][size];
-    Player player1;
-    Player player2;
+    Player *player1;
+    Player *player2;
 
 
     public:
