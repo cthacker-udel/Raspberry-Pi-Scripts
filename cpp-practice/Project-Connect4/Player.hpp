50f0b620ecf9004e7a55ea2702f71ae6ff67a20f
cthacker-udel <cthacker@udel.edu>
2021-07-28 04:31:37 -0400

    Added base classes for Connect4 in CPP

---
+++ cpp-practice/Project-Connect4/Player.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+class Player{
+
+    string name;
+    char piece;
+
+    public:
+
+        Player();
+
+        Player(string,char);
+
+};
