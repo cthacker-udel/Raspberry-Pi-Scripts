05cc251be2b4fe1b2968b863ad7c231c8cb5b83e
cthacker-udel <cthacker@udel.edu>
2021-07-28 04:16:53 -0400

    added block comment to evalBoard to specify directions to check and also added prototype for displayBoard method

--- connect4/board.c
+++ connect4/board.c
@@ -1,14 +1,42 @@
 #include "board.h"
 
 board *newBoard(int x, int y){
+    
+    if(x != y){
+        printf("\nMatrix must be same dimensions\n");
+        return NULL;
+    }
 
     char **newBoard = (char **)malloc((sizeof(char*) * x) * sizeof(char) * y);
     board *theBoard = (board *)malloc(sizeof(board));
+    theBoard->size = x;
     theBoard->theboard = newBoard;
     player *newPlayer1 = createPlayer();
     player *newPlayer2 = createPlayer();
     theBoard->player1 = newPlayer1;
     theBoard->player1 = newPlayer2;
     return theBoard;
+
+}
+
+int evalBoard(board *boardArg,int x,int y, int direction){
+
+    /*
+
+        1) North
+        2) North-East
+        3) East
+        4) South-East
+        5) South
+        6) South-West
+        7) West
+        8) North-West
+
+
+    */
+
+
+    
+
 
 }
--- connect4/board.h
+++ connect4/board.h
@@ -3,14 +3,18 @@
 typedef struct theBoard{
 
     char **theboard;
+    int size;
     player *player1;
     player *player2;
 
 }board;
 
 
-int evalBoard(board);
+int evalBoard(board*,int,int,int);
 
 
 board *newBoard(int,int);
+
+
+void displayBoard(board *);
 
