eb6898a24e42da32256546b1078f0abc6b87ee0a
cthacker-udel <cthacker@udel.edu>
2021-08-06 06:14:24 -0400

    added player creation methods

--- connect4/game.c
+++ connect4/game.c
@@ -7,6 +7,11 @@ int main(void){
     printf("\nEnter the size of the board, board will be NxN dimensions : ");
     scanf("%d",&theSize);
     board *iBoard = newBoard(theSize,theSize);
+    // board constructed
+
+    // create players
+    iBoard->player1 = createPlayer();
+    iBoard->player2 = createPlayer();
 
     while(1){
         // game loop
