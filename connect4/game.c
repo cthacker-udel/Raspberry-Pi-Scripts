1a4e47657a7a758c83ca960d6beb7aee66f777e6
cthacker-udel <cthacker@udel.edu>
2021-08-06 06:13:08 -0400

    added board construction before game loop

---
+++ connect4/game.c
@@ -0,0 +1,20 @@
+#include "board.h"
+#include "player.h"
+
+int main(void){
+
+    int theSize;
+    printf("\nEnter the size of the board, board will be NxN dimensions : ");
+    scanf("%d",&theSize);
+    board *iBoard = newBoard(theSize,theSize);
+
+    while(1){
+        // game loop
+
+
+
+
+    }
+
+}
+
