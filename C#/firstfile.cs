38f8e2ecebecc2b953c6614435bc2fd49551dec9
cthacker-udel <cthacker@udel.edu>
2021-08-07 05:28:42 -0400

    update

--- C#/firstfile.cs
+++ C#/firstfile.cs
@@ -1,27 +1,21 @@
 using System;
 
-/*
-
-Data Types
-
-*/
-
-sbyte sbyte1 = 0; // range -128 to 127
-byte byte1 = 0; // range from 0 to 255
-short short1 = 0; // range from -32768 to 32767
-ushort ushort1 = 0; // range from 0 to 65535
-int int1 = 0; // range from -2147483648
-uint uint1 = 0; // range from 0 to 4294967295
-long long1 = 0; // range from -9223372036854775808 to 9223372036854775807
-ulong ulong1 = 0; // range from 0 to 18446744073709551615
-float float1 = 0; // range from ±1.5×10-45  to ±3.4×1038
-double double1 = 0.0; // range from ±5.0×10-324 to ±1.7×10308
-decimal decimal1 = 0; // range from ±1.0×10-28 to ±7.9×1028
-bool bool1 = false;
-bool bool2 = true;
-char char1 = 'c';
-object obj;
-string str;
+public class Program{
 
 
-Console.WriteLine("Hello world");
+    static string MonthName(int num){
+
+        
+
+
+    }
+
+
+    public static void Main(string[] args){
+
+
+
+    }
+
+
+}
