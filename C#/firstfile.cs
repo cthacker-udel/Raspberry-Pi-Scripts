f421a19e24f78bad2aed3faa78c6659091ba6f0b
cthacker-udel <cthacker@udel.edu>
2021-08-07 04:51:52 -0400

    update

--- C#/firstfile.cs
+++ C#/firstfile.cs
@@ -1,3 +1,5 @@
+using System;
+
 /*
 
 Data Types
@@ -12,11 +14,14 @@ int int1 = 0; // range from -2147483648
 uint uint1 = 0; // range from 0 to 4294967295
 long long1 = 0; // range from -9223372036854775808 to 9223372036854775807
 ulong ulong1 = 0; // range from 0 to 18446744073709551615
-float float1 = 0.0; // range from ±1.5×10-45  to ±3.4×1038
+float float1 = 0; // range from ±1.5×10-45  to ±3.4×1038
 double double1 = 0.0; // range from ±5.0×10-324 to ±1.7×10308
-decimal decimal1 = 0.0; // range from ±1.0×10-28 to ±7.9×1028
+decimal decimal1 = 0; // range from ±1.0×10-28 to ±7.9×1028
 bool bool1 = false;
 bool bool2 = true;
 char char1 = 'c';
 object obj;
 string str;
+
+
+Console.WriteLine("Hello world");
