22a2c64f2d2ff837d9e09485719f4dea8a208b3b
cthacker-udel <cthacker@udel.edu>
2021-08-14 07:24:35 -0400

    added implementation for player.h class

---
+++ doggame/player.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+typedef struct thePlayer{
+
+    char *name;
+    int strength;
+    char firstInitial;
+    char lastInitial;
+
+}player;
+
+int getPlayerStrength(player *);
+
+player *createPlayer();
+
+char getPlayerFirstInitial(player *));
+
+char getPlayerLastInitial(player *);
