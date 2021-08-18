a74699dc4e83df2a7adf72a915545d6665a3767f
cthacker-udel <cthacker@udel.edu>
2021-08-18 07:14:31 -0400

    added prototypes to board.h

--- doggame/board.h
+++ doggame/board.h
@@ -1,1 +1,24 @@
 #include "player.h"
+
+typedef struct theBoard{
+
+    player player1;
+    player player2;
+    int size;
+    char defaultPiece;
+
+}board;
+
+board *makeBoard();
+
+player getPlayer1();
+
+player getPlayer2();
+
+int getSize();
+
+char getDefPiece();
+
+void setDefPiece(char);
+
+
