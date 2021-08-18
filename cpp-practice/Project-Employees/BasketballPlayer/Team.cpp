494d0b26a62245bfc9312e11cab15d524b6de035
cthacker-udel <cthacker@udel.edu>
2021-08-18 07:11:30 -0400

    added constructors for team class

--- cpp-practice/Project-Employees/BasketballPlayer/Team.cpp
+++ cpp-practice/Project-Employees/BasketballPlayer/Team.cpp
@@ -21,3 +21,14 @@ Team::Team(string newName, int newPlayers){
     this->playoffGamesWon = 0;
 
 }
+
+Team::Team(string newName, int newPlayers, int newWins, int newLosses, int newChampWons, int newPlayoffWons){
+
+    this->name = newName;
+    this->players = newPlayers;
+    this->wins = newWins;
+    this->losses = newLosses;
+    this->championshipsWon = newChampWons;
+    this->playoffGamesWon = newPlayoffWons;
+
+}
