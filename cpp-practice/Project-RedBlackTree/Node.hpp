e82d76cb9ea4e0a33240959ec12d42a23b3ab5c0
cthacker-udel <cthacker@udel.edu>
2021-08-23 04:26:59 -0400

    added base implementation for red-black tree

---
+++ cpp-practice/Project-RedBlackTree/Node.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string>
+#include <vector>
+#include <cmath>
+
+class Node{
+
+    int data; // int value for now, can be anything (double,float,string,etc)
+    char color; // R/B
+    Node *left;
+    Node *right;
+    Node *parent;
+
+};
