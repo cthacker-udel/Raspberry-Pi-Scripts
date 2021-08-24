3493077f9d31d76367c065cdd5b3170e31e63569
cthacker-udel <cthacker@udel.edu>
2021-08-24 14:53:35 -0400

    added base class for insertion sort

---
+++ cpp-practice/Project-SortingAlgos/InsertionSort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+using namespace std;
+
+
+class InsertionSort{
+
+    void insertionSort(int *, int);
+
+};
