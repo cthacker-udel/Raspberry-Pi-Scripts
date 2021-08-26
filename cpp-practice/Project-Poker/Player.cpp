39683bc9ed1f7bcb9af61c0426892416ce936943
cthacker-udel <cthacker@udel.edu>
2021-08-26 05:02:54 -0400

    added Player class

---
+++ cpp-practice/Project-Poker/Player.cpp
@@ -0,0 +1,9 @@
+#include "Player.hpp"
+
+Player::Player(string newName, int newWins, int newLosses){
+
+    this->name = newName;
+    this->wins = newWins;
+    this->losses = newLosses;
+
+}
---
+++ cpp-practice/Project-Poker/Player.hpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Player{
+
+    string name;
+    int wins;
+    int losses;
+
+    public:
+
+        Player(string,int,int);
+
+};
