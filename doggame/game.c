0001746926713e4af0f58e97926d31052ed82c76
cthacker-udel <cthacker@udel.edu>
2021-08-26 04:34:06 -0400

    added getting coords of board start and finish

--- doggame/game.c
+++ doggame/game.c
@@ -9,6 +9,27 @@ int main(void){
 
     srand(time(NULL));
     board *theboard = makeBoard();
+    // board constructed
+    int x = 0;
+    int y;
+    int boolVal = 0;
+    for(int i = 0; i < theboard->size; i++){
+
+        for(int j = 0; j < theboard->size; j++){
+
+            char theChar = *(*(theboard->theboard+i)+j);
+            if(theChar == 'E'){
+                y = i;
+                boolVal = 1;
+                break;
+            }
+
+        }
+        if(boolVal){
+            break;
+        }
+
+    }
     while(1){
 
         
