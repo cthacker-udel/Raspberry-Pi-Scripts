62d33b5dcb052d2f904dbb5cac85451769a29b7d
cthacker-udel <cthacker@udel.edu>
2021-08-30 06:15:48 -0400

    Added base structure definition for project.h

--- linkedlist/DoublyLinkedList/project.h
+++ linkedlist/DoublyLinkedList/project.h
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+typedef struct theNode{
+
+    int val;
+    struct theNode *next;
+
+}node;
+
+
 void addNode(int);
 
 void findFirstIndex(int);
 
 void findLastIndex(int);
+
+
 
