c24c95c908bf6ab6a3cbc2df8fd175efd4eec9dd
cthacker-udel <cthacker@udel.edu>
2021-08-30 06:19:01 -0400

    Added method to create new node

--- linkedlist/DoublyLinkedList/main.c
+++ linkedlist/DoublyLinkedList/main.c
@@ -10,5 +10,21 @@ void addNode(int value){
 }
 
 
+node *createNode(){
+
+    int val = -1;
+
+    do{
+        printf("\nEnter a integer value for val");
+    }while(scanf("%d",&val) == 0);
+
+    node *newNode = malloc(sizeof(node));
+    newNode->value = val;
+    return newNode;
+
+
+}
+
+
 
 
