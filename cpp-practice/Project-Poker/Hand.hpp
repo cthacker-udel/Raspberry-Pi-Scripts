6cdb8f62b76c19314cf779689473360d0d09ac6b
cthacker-udel <cthacker@udel.edu>
2021-09-04 10:09:28 -0400

    updated Hand.hpp

--- cpp-practice/Project-Poker/Hand.hpp
+++ cpp-practice/Project-Poker/Hand.hpp
@@ -4,6 +4,9 @@
 
 #ifndef HAND_HPP
 #define HAND_HPP
+
+#pragma once
+
 #include <vector>
 #include "Card.hpp"
 #include <list>
@@ -19,15 +22,14 @@ public:
 
     Hand(vector<Card>);
 
-
     Card *deal();
 
     vector<Card *> dealN(int);
-
+    
     void createDeck();
-
+    
     void shuffle(int);
-
+    
     vector<Card> getHand();
 
 };
