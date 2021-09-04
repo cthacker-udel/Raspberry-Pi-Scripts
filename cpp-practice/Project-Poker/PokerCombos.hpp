421dd810139ba2335fd3cfd4aca581baccee8abf
cthacker-udel <cthacker@udel.edu>
2021-09-04 10:10:32 -0400

    added PokerCombos.hpp

---
+++ cpp-practice/Project-Poker/PokerCombos.hpp
@@ -0,0 +1,51 @@
+
+#ifndef POKER_COMBOS_HPP
+#define POKER_COMBOS_HPP
+
+#pragma once
+
+#include "Hand.hpp"
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+class PokerCombos{
+	
+	public:
+		
+		int findHighCard(Hand,int);
+		
+		bool isThreeOfAKind(Hand);
+		
+		bool isTwoPair(Hand);
+		
+		bool isPair(Hand);
+		
+		bool isStraight(Hand);
+		
+		bool isFullHouse(Hand);
+		
+		bool isFourOfAKind(Hand);
+		
+		bool isStraightFlush(Hand);
+		
+		int findLowCard(Hand);
+		
+		vector<int> sortNumRanks(Hand);
+		
+		int findNumSuit(Hand,string);
+		
+		int findNumKinds(Hand,int);
+		
+		bool isSameSuit(Hand);
+		
+		bool isAlternating(Hand);
+		
+		bool isRoyalFlush(Hand);
+	
+	
+};
+
+#endif
